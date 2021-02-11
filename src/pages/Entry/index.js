001f2f2fe004715fcacf8f8f96a451ae83904b59
raman32 <rg.raman.ghimire@gmail.com>
2021-02-11 20:15:14 +0545

    added useName hooks

--- src/pages/Entry/index.js
+++ src/pages/Entry/index.js
@@ -1,27 +1,32 @@
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
-import {routes } from '../../utils/constants'
+import { Link } from 'react-router-dom'
+import { routes } from '../../utils/constants'
 import './index.css'
 import TextBox from '../../components/TextBox'
 import DropDownList from '../../components/DropDownList'
 import keyboardIcon from '../../assets/icons/keyboard-icon.svg'
 import playIcon from '../../assets/icons/play-icon.svg'
 import Button from '../../components/Button'
-import {difficulties} from '../../utils/difficulty'
-function Entry(){
-const [playerName,setPlayerName] =useState('');
-const [difficulty, setDifficulty] = useState(1);
-return (
-    <div className="Entry">
-       <img className="keyboardLogo" src={keyboardIcon} alt="logo" />   
-       <div className="logoTitle"> fast finger</div>
-       <div className="logoDescription">____________the ultimate typing game___________</div>
-        <TextBox text={playerName} setText={setPlayerName} placeholder="Type your name"/>
-        <DropDownList items={difficulties} setItem={setDifficulty} placeholder="Difficulty Level"/>
-        <Link to={`${routes.game}/${playerName}/${difficulty}`} >
-            <Button icon={playIcon} text="Start Game" />
-        </Link>
-    </div>
-);
-}                           
+import { difficulties } from '../../utils/difficulty'
+import useName from '../../hooks/useName'
+
+function Entry() {
+    const { name, setName, emptyError } = useName('');
+    const [difficulty, setDifficulty] = useState(1);
+
+
+    return (
+        <div className="Entry">
+            <img className="keyboardLogo" src={keyboardIcon} alt="logo" />
+            <div className="logoTitle"> fast finger</div>
+            <div className="logoDescription">____________the ultimate typing game___________</div>
+            <TextBox text={name} setText={setName} placeholder="Type your name" isError={emptyError} />
+            {emptyError ? <div> Please Enter your Name </div> : null}
+            <DropDownList items={difficulties} setItem={setDifficulty} placeholder="Difficulty Level" />
+            <Link to={`${routes.game}/${name}/${difficulty}`} >
+                <Button icon={playIcon} text="Start Game" />
+            </Link>
+        </div>
+    );
+}
 export default Entry
